eb76462655acbc1ffa954682742bb55a802541a3
lixinjian <lixinjian1217@gmail.com>
2022-09-01 11:55:26 -0400

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
    name='phonepiece',
-   version='1.0',
+   version='1.0.1',
    description='a multilingual phone tokenizer',
    author='Xinjian Li',
    author_email='xinjianl@cs.cmu.edu',
