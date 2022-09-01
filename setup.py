e4beaee7deef01dc0ac9ce733f5c6b4c7bf22146
lixinjian <lixinjian1217@gmail.com>
2022-09-01 11:54:56 -0400

    fix download link

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
    name='phonepiece',
-   version='0.0',
+   version='1.0',
    description='a multilingual phone tokenizer',
    author='Xinjian Li',
    author_email='xinjianl@cs.cmu.edu',
