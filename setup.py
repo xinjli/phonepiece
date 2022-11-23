7465d20797305a1673390222e2ad5417acfe6c67
lixinjian <lixinjian1217@gmail.com>
2022-11-23 14:23:06 -0500

    add pinyin2ipa to package

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ requirements=[r.strip() for r in open("requirements.txt").readlines()]
 
 setup(
    name='phonepiece',
-   version='1.1.1',
+   version='1.1.2',
    description='a multilingual phone tokenizer',
    author='Xinjian Li',
    author_email='xinjianl@cs.cmu.edu',
    url="https://github.com/xinjli/phonepiece",
    packages=find_packages(),
    install_requires=requirements,
-   package_data={'': ['*.csv', '*.tsv', 'tree.txt']},
+   package_data={'': ['*.csv', '*.tsv', 'tree.txt', 'pinyin2ipa.txt']},
    include_package_data=True,
 )
